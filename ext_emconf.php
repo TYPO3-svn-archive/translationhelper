6fc473ea69b9a335622b90b533ce8277be0d47ea
sorenmalling <sorenmalling@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-07-27 05:55:56 +0000

    Added ext_icon.gif - no functional change
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/translationhelper/trunk@50158 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "translationhelper".
 #
-# Auto generated 25-07-2011 22:22
+# Auto generated 27-07-2011 07:47
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'version' => '1.0.1',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"f71d";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"6871";s:20:"doc/wizard_form.html";s:4:"dbc8";}',
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"f71d";s:10:"README.txt";s:4:"ee2d";s:17:"class.ux_lang.php";s:4:"921f";s:21:"ext_conf_template.txt";s:4:"1b83";s:12:"ext_icon.gif";s:4:"5456";s:17:"ext_localconf.php";s:4:"2587";s:13:"locallang.xml";s:4:"bba9";s:14:"doc/manual.sxw";s:4:"f04f";s:19:"doc/wizard_form.dat";s:4:"6871";s:20:"doc/wizard_form.html";s:4:"dbc8";}',
+	'suggests' => array(
+	),
 );
 
 ?>
